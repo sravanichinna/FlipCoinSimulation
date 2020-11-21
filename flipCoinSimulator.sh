15c563e1119294e4d3540c5b3d379526646eebd5
sravanichinna <sravanidane413.com>
2020-11-21 14:50:37 +0530

    Added: MultipleTimeOfFlipCoin logic

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,12 +2,19 @@
 echo "welcome to flip coin simulation"
 headCount=0
 tailCount=0
+while(( $headCount != 21 && $tailCount != 21 ))
+do
 
+	flip=$(( $RANDOM%2 ))
+	if [[$flip -eq 1 ]]
+	then
+		echo -n "heads"
+		((headCount++))
+	else
+		echo -n "tails"
+		((tailCount++))
+	fi
+done
+echo "total no of heads:" $headCount
+echo "total no of tails:" $tailCount
 
-flip=$(( $RANDOM%2 ))
-if [[$flip -eq 1 ]]
-then
-	echo -n "heads"
-else
-	echo -n "tails"
-fi
